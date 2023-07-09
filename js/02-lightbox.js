154af24bd6d8625824b9d1f21ff2c4105e4e33cf
Slav-0N <SOVILGO@gmail.com>
2023-07-09 16:22:06 +0300

    2nd Task is ready

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,31 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+const galleryElem = document.querySelector(".gallery");
+
+const imageListArr = galleryItems.map(item => {
+  return `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        
+        alt="${item.description}"
+      />
+    </a>
+  </li>`
+});
+
+
+const template = imageListArr.join('');
+galleryElem.insertAdjacentHTML("afterbegin", template);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: "alt", 
+  captionDelay: 250,
+});
+
+
