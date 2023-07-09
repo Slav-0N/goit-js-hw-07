7a8196cabea7af9e4e0790d94ca0131bfbcb4b50
Slav-0N <SOVILGO@gmail.com>
2023-07-09 14:00:50 +0300

    1st Task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,48 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryElem = document.querySelector(".gallery");
+
+const imageListArr = galleryItems.map(item => {
+  return `<li><img class="previewPicture" src="${item.preview}" data-src="${item.original}" alt="${item.description}" width="340px"></li>`
+});
+
+const template = imageListArr.join('');
+galleryElem.insertAdjacentHTML("afterbegin", template);
+
+galleryElem.addEventListener('click', delegation);
+
+function delegation(event) {
+  if (event.target.tagName === 'IMG') {
+    const origPic = event.target.dataset.src;
+    console.log(origPic);
+
+    //  приклад Images з сайту https://basiclightbox.electerious.com/
+    
+    const instance = basicLightbox.create(`
+    <img src="${origPic}" width="800" height="600">
+`)
+    instance.show()
+  };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
