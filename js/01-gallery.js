06f66fb962c52d0b006bb5dfd00ab45b4bfb92b8
Slav-0N <SOVILGO@gmail.com>
2023-07-09 15:20:22 +0300

    1st Task 7th Module
    Переробив згідно  вимог в ДЗ (назви класів, атрибути, додав ескейп

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,7 +7,16 @@ console.log(galleryItems);
 const galleryElem = document.querySelector(".gallery");
 
 const imageListArr = galleryItems.map(item => {
-  return `<li><img class="previewPicture" src="${item.preview}" data-src="${item.original}" alt="${item.description}" width="340px"></li>`
+  return `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>`
 });
 
 const template = imageListArr.join('');
@@ -16,18 +25,35 @@ galleryElem.insertAdjacentHTML("afterbegin", template);
 galleryElem.addEventListener('click', delegation);
 
 function delegation(event) {
+  event.preventDefault();
+  
   if (event.target.tagName === 'IMG') {
-    const origPic = event.target.dataset.src;
+    const origPic = event.target.dataset.source;
     console.log(origPic);
 
     //  приклад Images з сайту https://basiclightbox.electerious.com/
     
     const instance = basicLightbox.create(`
     <img src="${origPic}" width="800" height="600">
-`)
+`);
     instance.show()
+
+    document.addEventListener('keydown', keyCloseLitebox, {once: true});
+     // чи вірно я зрозумів, що обробник додається цим розгалуженням "if" та після першого виконання, буде видалений? 
+
+    function keyCloseLitebox(event) {
+      console.log(event);
+      if (event.code === 'Escape') {
+        instance.close()
+      };
+    };
+
   };
 };
+
+
+
+
 
 
 
